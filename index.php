abce1dde07ab123abbaff12fb6aa82a8773bc5eb
khairulbosunia <39398292+khairulbosunia@users.noreply.github.com>
2018-05-20 11:33:23 +0600

    first

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>php basic</title>
+</head>
+<body>
+	
+	<?php 
+		$khairul='bosunia';
+		
+		$sity = array('rongpur' , 'kurigram' , 'dhaka' , 'lalmonihat');
+	?>
+	
+	<?php 
+	if($khairul=='bosunia'){
+		echo '<h1>my name is khairul islam bosunia</h1>';
+	}
+	else{
+		echo '<h3>my name is  babu bosunia</h3>';
+	}
+	?>
+	
+	<?php 
+	
+	echo '<h3>khairul islam bosunia'.$sity[1].'</h3>';
+	?>
+	
+	<?php 
+		if($sity[2]=='lalmonihat'){
+			echo 'bangladeshe rajdani dhaka';
+		}
+		
+		else{
+			echo 'amar bari kurigram';
+		}
+	?>
+	
+</body>
+</html>
